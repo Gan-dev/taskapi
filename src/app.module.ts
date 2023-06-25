690bbc6a59487bd4cb48897bfc1d86d643fcc88f
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-06-25 16:51:16 +0200

    version 0.0.0.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://gandev:gandev.2023@cluster0.3eyakps.mongodb.net/?retryWrites=true&w=majority'),
-    TasksModule
+    TasksModule,
+    ConfigModule.forRoot()
   ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: process.env.FRONT_END_URL,
+    credentials: false,
+  })
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
 
